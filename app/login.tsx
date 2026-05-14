1f33c380077763eb9f8107d05353c08d43eee890
dimashmuel <dimashmuel12@gmail.com>
2026-05-14 15:05:03 +0300

    login page

--- app/login.tsx
+++ app/login.tsx
@@ -1,3 +1,18 @@
+import "./App.css";
+
 export default function Login() {
-    return <h1>Login Page</h1>;
+    return(
+        <div>
+            <h1 style={{color: 'green'}}>Login Page</h1>
+            <label >Username:
+                <input type="text"/>
+            </label><br/>
+            <label>Password:
+                <input/>
+            </label><br/>
+            <button onClick={()=> {alert('You clicked the login button!')}}>Login</button>
+            <p className='small'> if you don't have an account, SignUp<br /> Forget Password </p>
+        </div>
+        
+    );
 }
