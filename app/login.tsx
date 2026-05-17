6a0b941516df7d16a216d0cee37b3fac9824a4c9
dimashmuel <dimashmuel12@gmail.com>
2026-05-17 18:47:35 +0300

    Add hooks to login page

--- app/login.tsx
+++ app/login.tsx
@@ -1,18 +1,40 @@
 import "./App.css";
+import { useState } from 'react';
+
 
 export default function Login() {
-    return(
-        <div>
-            <h1 style={{color: 'green'}}>Login Page</h1>
-            <label >Username:
-                <input type="text"/>
-            </label><br/>
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleEmail(e: React.ChangeEvent<HTMLInputElement>) {
+        setEmail(e.target.value);
+    }
+
+    function handlePassword(e: React.ChangeEvent<HTMLInputElement>) {
+        setPassword(e.target.value);
+    }
+    console.log('Email:', email);
+    console.log('Password:', password);
+
+    return (
+        <form onSubmit={e => e.preventDefault()}>
+            <h1 style={{ color: 'green' }}>Login Page</h1>
+            <label>Email:
+                <input
+                    type="email"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={handleEmail} />
+            </label><br />
             <label>Password:
-                <input/>
-            </label><br/>
-            <button onClick={()=> {alert('You clicked the login button!')}}>Login</button>
+                <input
+                    type="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={handlePassword} />
+            </label><br />
+            <button type="submit">Login</button>
             <p className='small'> if you don't have an account, SignUp<br /> Forget Password </p>
-        </div>
-        
+        </form>
     );
 }
