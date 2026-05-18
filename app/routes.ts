c621a60ced680559a0f65f9532b62adc49017e66
dimashmuel <dimashmuel12@gmail.com>
2026-05-18 17:35:39 +0300

    Rename file.tsx → home.tsx and the component to Home. Update routes.ts accordingly.

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,7 @@
 import { type RouteConfig, route } from "@react-router/dev/routes";
 
 export default [
-    route("/", "file.tsx"),
+    route("/", "home.tsx"),
     route("/login", "login.tsx"),
     route("/register", "register.tsx"),
 ] satisfies RouteConfig;
